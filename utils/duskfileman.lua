0308cafc4ccef26e7f4a9a60d7d97f1071d13750
XDuskAshesReal <101196233+XDuskAshes@users.noreply.github.com>
2024-03-15 12:30:49 -0400

    [dfm]: Response messages and clear-on-exit
    Response messages:
        Using function 'displayCustomMessage()' will display custom orange text at terminal bottom.
    
    Clear-on-exit:
        Changing value 'clrOnExit' to 'true' will clear the screen on exiting dfm.

--- utils/duskfileman.lua
+++ utils/duskfileman.lua
@@ -8,6 +8,7 @@ local args = {...}
 
 local behavior = {}
 behavior.onFileEntered = "edit"
+behavior.clrOnExit = false
 
 -- The above has two values possible.
 -- 'edit' - Opens the file in 'edit'.
@@ -21,9 +22,7 @@ local function e(s)
     return #s < 1
 end
 
-local function topLine(string)
-    local txm, tym = term.getSize()
-    
+local function topLine(string)    
     write(string.."\n")
 
     -- Should create a line that's only as long as the given string.
@@ -32,6 +31,25 @@ local function topLine(string)
     end
 
     write("\n")
+end
+
+local function displayCustomMessage(text) -- Display a custom message at 1,19 in orange.
+    local txm, tym = term.getSize()
+
+    term.setCursorPos(1,tym)
+
+    if #text > txm then
+        term.setTextColor(colors.red)
+        write("Could not display message, too big.")
+        sleep(1)
+    else
+        term.setTextColor(colors.orange)
+        write(text)
+    end
+    
+    term.setTextColor(colors.white)
+    sleep(0.5)
+    term.clearLine()
 end
 
 if args[1] == "--help" or args[1] == "-h" then
@@ -47,7 +65,7 @@ else
     end
 
     while true do
-        
+
         term.clear()
         term.setCursorPos(1,1)
 
@@ -69,13 +87,18 @@ else
             end
         end
 
-        write("\n\n} ")
+        term.setCursorPos(1,18)
+        write("} ")
         -- TODO: Autocomplete. -Dusk, 3/15/2024
         local input = read()
 
         if fs.exists(shell.dir().."/"..input) and fs.isDir(shell.dir().."/"..input) then
             shell.setDir(shell.dir().."/"..input)
         elseif input == "exit" then
+            if clrOnExit then
+                term.clear()
+                term.setCursorPos(1,1)
+            end
             error()
         elseif input == "info" then
             term.clear()
@@ -96,8 +119,10 @@ else
             if behavior.onFileEntered == "edit" then
                 print(shell.dir().."/"..input)
                 shell.run("edit /"..shell.dir().."/"..input)
+                displayCustomMessage("File viewed or modified.")
             else
                 shell.run("/"..shell.dir().."/"..input)
+                displayCustomMessage("Ran file that was selected.")
             end
         end
     end
